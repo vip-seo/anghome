afaf21e4864ad2807fec6eab967e01d329f2a7e0
vip-seo <csat.vip@gmail.com>
2019-12-24 21:07:25 +0200

    some

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
   title = 'app';
+
+  public showId (idx: number):void  {
+    console.log(idx);
+  }
+
 }

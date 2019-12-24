7b692a7d4f84745ba555becf1487ee5969c171e2
vip-seo <csat.vip@gmail.com>
2019-12-24 19:12:03 +0200

    some

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyTableComponent } from './my-table/my-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyTableComponent
   ],
   imports: [
     BrowserModule,

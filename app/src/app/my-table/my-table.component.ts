2478b070518c5e59e4a3dca028bde3bf7a85bad4
vip-seo <csat.vip@gmail.com>
2019-12-25 15:44:08 +0200

    some

--- app/src/app/my-table/my-table.component.ts
+++ app/src/app/my-table/my-table.component.ts
@@ -9,7 +9,7 @@ const products =  [
   {id: 4, name : 'product 4', price : 400, category: 'Category 2' }, 
   {id: 5, name : 'product 5', price : 500, category: 'Category 3' }, 
   {id: 6, name : 'product 6', price : 600, category: 'Category 2' }, 
-  {id: 7, name : 'product 7', price : 700, category: 'Category 2' }, 
+  {id: 7, name : 'product 7', price : 700, category: 'Category 4' }, 
   {id: 8, name : 'product 8', price : 800, category: 'Category 2' },
   {id: 9, name : 'product 9', price : 900, category: 'Category 3' },
   {id: 10, name : 'product 10', price : 1000, category: 'Category 1' }
@@ -23,6 +23,7 @@ const products =  [
 export class MyTableComponent implements OnInit {
   
   public productList = products;  
+  public productList1 = products;  
 
   @Input() public rows: number;
 
@@ -30,15 +31,45 @@ export class MyTableComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log(this.rows);
+    
   }
   
   public deleteRow(id: number) {
     this.deleteProduct.emit(id);
     this.productList = this.productList.filter((p) => p.id !== id);
+    //Если удаляем то уже навсегда для фильтрации All category
+    this.productList1 = this.productList;
 
   }
   public getList(): any {
     return this.productList.filter((item, index) => this.rows > index);
+  }
+
+  public filterCat(value: string) {
+    console.log(value);
+    if (value !== 'All category'){
+    this.productList = this.productList.filter((p) => p.category == value);
+    } else {
+      this.productList = this.productList1;
+    }
+  }
+
+  public getCategory(): any {
+    let sortArr: string[] = ['All category'];
+        
+      for (var i = 0; i < this.productList.length; i++) 
+        {
+          let status: boolean = false;
+          for (var y = 0; y < sortArr.length; y++) 
+            {
+              if (this.productList[i].category == sortArr[y]) { 
+                status = true; 
+              }
+            }
+          if (!status) {
+            sortArr.push(this.productList[i].category);
+          }
+        }
+    return sortArr;
   }
 }
